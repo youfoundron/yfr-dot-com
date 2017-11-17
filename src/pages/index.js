fcb3093dd46eae9a2d9d220680a7d5ee01a3383c
Ron Gierlach <rongierlach@gmail.com>
2017-11-17 07:00:41 -0600

    fill out home page content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,17 @@
 import React from 'react'
-// import Link from 'gatsby-link'
+import Link from 'gatsby-link'
 
 import { head } from '../content/home'
 import Head from '../components/Head'
 import Container from '../components/Container'
+
+const Copy = props => (
+  <p className='f5 f4-ns lh-copy' {...props} />
+)
+
+const Callout = props => (
+  <Link className='sans-serif link fw9 black hover-gold' {...props} />
+)
 
 const HomePage = () => (
   <Container>
@@ -11,7 +19,18 @@ const HomePage = () => (
     <section className='pv5 center measure georgia'>
       <h1 className='sans-serif'>Found Me.</h1>
       <hr />
-
+      <Copy>
+        I'm a Chicago based software developer, currently writing web apps and smart contracts on Ethereum.
+      </Copy>
+      <Copy>
+        I've done varied <Callout to='/work'>work</Callout> for some pretty interesting clients.
+      </Copy>
+      <Copy>
+        Sparingly, I may <Callout to='/blog'>write</Callout> about programming and related topics.
+      </Copy>
+      <Copy>
+        If you're interested in working together, or just feel like connecting, let's <Callout to='/contact'>get in touch</Callout>.
+      </Copy>
     </section>
   </Container>
 )
