b3175c8f9f68831f15000cac31918803a13cdd57
Ron Gierlach <rongierlach@gmail.com>
2017-11-17 07:45:03 -0600

    fill out /contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,14 +3,15 @@ import React from 'react'
 import { head } from '../content/contact'
 import Head from '../components/Head'
 import Container from '../components/Container'
+import ContactForm from '../components/ContactForm'
 
 const ContactPage = () => (
   <Container>
     <Head {...head} />
-    <section className='pv5 center measure georgia'>
+    <section className='pv5 center measure'>
       <h1 className='sans-serif'>Contact</h1>
       <hr />
-
+      <ContactForm />
     </section>
   </Container>
 )
