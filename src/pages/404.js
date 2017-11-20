d270672d87df0a7f1f67f7fd2cc08122e79cc632
Ron Gierlach <rongierlach@gmail.com>
2017-11-19 19:39:03 -0600

    404 page

---
+++ src/pages/404.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Container from '../components/Container'
+
+const NotFound = () => (
+  <Container>
+    <section className='pv5 measure center'>
+      <h1 className='tc tracked-tight f-headline lh-solid mb0'>404</h1>
+      <p className='tc georgia f5 f4-ns lh-copy'>Nothing to see here!</p>
+    </section>
+  </Container>
+)
+
+export default NotFound
