ef973570f54832e59e4c6765397bbec3e47f0f5d
Ron Gierlach <rongierlach@gmail.com>
2017-11-24 10:16:25 -0600

    remove react-perfect-scrollbar

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,20 +1,17 @@
 import React from 'react'
-import Scrollbar from 'react-perfect-scrollbar'
 
 import '../styles/index.scss'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 
 const Layout = ({ children }) => (
-  <Scrollbar className='sans-serif near-black bg-near-white'>
-    <div className='flex flex-column flex-auto min-vh-100'>
-      <Header />
-      <main className='flex-auto'>
-        { children() }
-      </main>
-      <Footer />
-    </div>
-  </Scrollbar>
+  <div className='flex flex-column flex-auto min-vh-100 sans-serif near-black bg-near-white'>
+    <Header />
+    <main className='flex-auto'>
+      { children() }
+    </main>
+    <Footer />
+  </div>
 )
 
 export default Layout
